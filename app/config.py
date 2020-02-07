ead3f21c474bdbf613de05a5c73cbf4daba18d3d
wanjiiru <wnganga@farmdrive.co.ke>
2020-02-07 12:14:04 +0300

    add migrations

--- app/config.py
+++ app/config.py
@@ -1,8 +1,18 @@
 import os
 
+POSTGRES = {
+    'user': os.getenv('POSTGRES_USER', 'postgres'),
+    'pw': os.getenv('POSTGRES_PASSWORD', 'password'),
+    'db': os.getenv('POSTGRES_DB', 'sales'),
+    'host': os.getenv('POSTGRES_HOST', 'localhost'),
+    'port': os.getenv('POSTGRES_PORT', 5432)
+
+
+}
 
 class Config(object):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://{user}:{pw}@{host}:{port}/{db}'.format(**POSTGRES))
 
 
 
